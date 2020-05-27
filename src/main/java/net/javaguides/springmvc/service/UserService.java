a7eb08f0d1c9cee1b4996430e6ae40262c46b59c
Christos Ponirakos <cponirakos@gmail.com>
2020-05-27 20:57:37 +0300

    updating user -service-controller

---
+++ src/main/java/net/javaguides/springmvc/service/UserService.java
@@ -0,0 +1,17 @@
+package net.javaguides.springmvc.service;
+
+import java.util.List;
+
+import net.javaguides.springmvc.entity.User;
+
+public interface UserService {
+
+	public List<User> findAll();
+
+//	public void saveCustomer(Customer theCustomer);
+//
+//	public Customer getCustomer(int theId);
+//
+//	public void deleteCustomer(int theId);
+	
+}
---
+++ src/main/java/net/javaguides/springmvc/service/UserServiceImpl.java
@@ -0,0 +1,46 @@
+package net.javaguides.springmvc.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import net.javaguides.springmvc.dao.UserDaoImpl;
+import net.javaguides.springmvc.entity.User;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserDaoImpl dao;
+	
+	@Override
+	@Transactional
+	public List<User> findAll() {
+		return dao.findAll();
+	}
+
+//	@Override
+//	@Transactional
+//	public void saveCustomer(Customer theCustomer) {
+//		customerDAO.saveCustomer(theCustomer);
+//	}
+//
+//	@Override
+//	@Transactional
+//	public Customer getCustomer(int theId) {
+//		return customerDAO.getCustomer(theId);
+//	}
+//
+//	@Override
+//	@Transactional
+//	public void deleteCustomer(int theId) {
+//		customerDAO.deleteCustomer(theId);
+//	}
+}
+
+
+
+
+
